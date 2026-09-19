c1e35d383962983a436ebb29e57cb9dbaeec9c24
Adhi2312 <135993267+Adhi2312@users.noreply.github.com>
2026-09-19 12:36:05 +0530

    Add: generate-parentheses - submission-1

---
+++ Data Structures & Algorithms/generate-parentheses/submission-1.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    vector<string>ans;
+    bool isValid(string s)
+    {
+        int open=0;
+        int close=0;
+        for(char c:s)
+        {
+            if(c=='(')
+            {
+                open++;
+            }
+            else
+            {
+                if(open<1)return false;
+                open--;
+            }
+        }
+        return open==0 && close ==0;
+    }
+    void generate(int n,string s,int open,int close)
+    {
+        if(open < 0 || close < open) return;
+        if(s.size()==2*n)
+        {
+            
+            ans.push_back(s);
+            
+            return;
+        }
+        
+            generate(n,s+'(',open-1,close);
+        
+        generate(n,s+')',open,close-1);
+
+    }
+    vector<string> generateParenthesis(int n) {
+        generate(n,"",n,n);
+        return ans;
+        
+        
+    }
+};
